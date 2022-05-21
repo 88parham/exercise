5f057be141c32758bdd13d659dedcc2b63808e30
Parham <93513876+88parham@users.noreply.github.com>
2022-05-21 16:21:27 +0430

    Add files via upload

---
+++ exercise/javaescript.js
@@ -0,0 +1,12 @@
+// *** Submit function start here *** // 
+SubmitEvent.onclick = function() {
+  document.getElementById ("firstName")
+  document.getElementById ("lastName")
+  document.getElementById ("nationalId")
+  document.getElementById ("cellPhone")
+  return "firstName","lastName","nationalId","cellPhone"
+  
+}
+
+  var fn = document.getElementById("firstName").value;
+  document.getElementById("???").innerHTML = "fn";
